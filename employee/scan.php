4391144c6f15efae616f561aefdc49986d8703a9
Mongkol <mongkolkamsu@gmail.0com>
2026-07-31 10:01:30 +0700

    แก้กล้องสแกน 5

--- employee/scan.php
+++ employee/scan.php
@@ -246,7 +246,7 @@ try {
         </div>
 
         <!-- ส่วนปุ่มควบคุมการบันทึกข้อมูลเวลาทำงาน -->
-        <div class="pt-2 space-y-2">
+        <div class="pt-2 pb-16 space-y-2">
             <button id="btnRetake" class="w-full hidden bg-white border border-slate-200 hover:bg-slate-50 text-slate-600 font-medium py-2.5 rounded-2xl text-xs tracking-wide transition-all active:scale-[0.98] cursor-pointer items-center justify-center gap-2 shadow-xs">
                 <svg class="w-4 h-4 text-slate-400 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"></path>
